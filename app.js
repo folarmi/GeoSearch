6aa70de54304793a971c8ac7886af87625d4efeb
folarmi <fola4ril@gmail.com>
2020-04-03 13:56:00 -0700

    fixed desktop version

--- app.js
+++ app.js
@@ -44,8 +44,61 @@ function initAutocomplete() {
 
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
+
     places.forEach(function(place) {
-      // console.log(place.photos[1][0])
+
+      let coordinates = {
+        latitude : place.geometry.location.lat().toFixed(4),
+        longitude : place.geometry.location.lng().toFixed(4)
+      }
+
+      const latitude = coordinates.latitude;
+      const longitude = coordinates.longitude;
+
+
+
+      // Get Weather
+      document.getElementById('show-weather').addEventListener('click', getWeather);
+
+      function getWeather () {
+      var api = ' https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&APPID=78c41b3ef7b008eb035fd1e2cfa87696';
+
+
+      fetch(api)
+      .then((res) => res.json())
+              .then((data) => {
+              var humidity = data.main.humidity;
+              var windSpeed = data.wind.speed;
+              var Temperature = Math.round(data.main.temp - 273.15); 
+              var weather = data.weather[0].description;
+              var maxTemp= Math.round(data.main.temp_max - 273.15)
+              var pressure = data.main.pressure;
+              var name = data.name;
+              var iconCode = data.weather[0].icon;
+              var iconurl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+
+            //To get Date in Js
+              var date = new Date();
+              var currentDay = date.getDate();
+              var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+              var currentMonth = months[date.getMonth()];
+
+            // Posts to the DOM
+              nameOfCity.innerHTML = name;
+              humidityValue.innerHTML = humidity + '%';
+              windSpeedValue.innerHTML = windSpeed + "km/hr";
+              pressureValue.innerHTML = pressure;
+              maxTempValue.innerHTML = maxTemp + "&#8451" ;
+              temperatureValue.innerHTML = Temperature + "&#8451";
+              weatherDescription.innerHTML = currentMonth +  " " + currentDay + " | " + weather;
+              weatherImage.setAttribute('src',iconurl)
+            })
+        }
+
+
+
+
+
       if (!place.geometry) {
         console.log("Returned place contains no geometry");
         return;
@@ -65,55 +118,19 @@ function initAutocomplete() {
         title: place.name,
         position: place.geometry.location
       }));
-      
+
+    
 
       if (place.geometry.viewport) {
         // Only geocodes have viewport.
-        console.log(place)
         bounds.union(place.geometry.viewport);
       } else {
         bounds.extend(place.geometry.location);
       }
     });
     map.fitBounds(bounds);
+    
   });
+
 }
 
-
-//Get Weather
-document.getElementById('show-weather').addEventListener('click', getWeather);
-
-function getWeather () {
-  const cityName = document.getElementById('pac-input').value;
-    var api = ' https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&APPID=78c41b3ef7b008eb035fd1e2cfa87696';
-
-    fetch(api)
-   .then((res) => res.json())
-   .then((data) => {
-      var humidity = data.main.humidity;
-      var windSpeed = data.wind.speed;
-      var Temperature = Math.round(data.main.temp - 273.15); 
-      var weather = data.weather[0].description;
-      var maxTemp= Math.round(data.main.temp_max - 273.15)
-      var pressure = data.main.pressure;
-      var name = data.name;
-      var iconCode = data.weather[0].icon;
-      var iconurl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-
-      //To get Date in Js
-      var date = new Date();
-      var currentDay = date.getDate();
-      var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      var currentMonth = months[date.getMonth()];
-
-      // Posts to the DOM
-      nameOfCity.innerHTML = name;
-      humidityValue.innerHTML = humidity;
-      windSpeedValue.innerHTML = windSpeed + "km/hr";
-      pressureValue.innerHTML = pressure;
-      maxTempValue.innerHTML = maxTemp;
-      temperatureValue.innerHTML = Temperature + "&#8451";
-      weatherDescription.innerHTML = currentMonth +  " " + currentDay + " | " + weather;
-      weatherImage.setAttribute('src',iconurl)
-})
-}
